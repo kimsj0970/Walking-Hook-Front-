8336d616aaae0dab1054f71c6755d878be5ec80c
kimsj0970 <kimsj0970@gmail.com>
2026-05-26 15:19:19 +0900

    [CHORE] Vite 환경 변수 타입 및 프록시 설정 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true,   // TODO ngrok 도메인 허용
+    cors: true,
+  },
 })
