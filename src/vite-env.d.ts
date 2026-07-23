2b7b5bb5b020c03d05a6907d0c87c1f5f28ead96
kimsj0970 <kimsj0970@gmail.com>
2026-07-23 18:41:59 +0900

    [CHORE] VITE_GA_MEASUREMENT_ID 환경변수 타입 정의 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_KAKAO_REST_API_KEY: string;
   readonly VITE_KAKAO_REDIRECT_URI: string;
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_GA_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
